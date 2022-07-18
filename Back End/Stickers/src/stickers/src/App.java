82dbda4830d3faedb74a86e412604a900b9058db
Redyf <mateusalvespereira7@gmail.com>
2022-07-18 17:34:40 -0300

    update Imersão

--- Back End/Stickers/src/stickers/src/App.java
+++ Back End/Stickers/src/stickers/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,10 +15,19 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
 
         // extrair só os dados que interessam (título, poster, classificação)
+        var Parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = Parser.parse(body);
 
         // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+
         }
     }
+}
