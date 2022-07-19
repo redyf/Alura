60d0fa42d389a3115dc97d0e58a5772c4631622e
Redyf <mateusalvespereira7@gmail.com>
2022-07-19 18:50:39 -0300

    update

--- Back End/GeradoraDeFigurinhas.java
+++ Back End/GeradoraDeFigurinhas.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+
 import javax.imageio.ImageIO;
 public class GeradoraDeFigurinhas {
 
